7f2d3c0b24e637e3f90cfe81670318b42fd24f4a
jesus lopez <jelz2yk@hotmail.com>
2018-04-13 13:22:44 +0300

    toogle

--- src/app/configuracion/configuracion-routing.module.ts
+++ src/app/configuracion/configuracion-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReglasComponent } from './reglas/reglas/reglas.component';
 import { MenuComponent } from './menu/menu/menu.component';
+import { GroupsComponent } from './groups/groups/groups.component';
 
 const routes: Routes = [
   { path: 'Reglas', component: ReglasComponent },
-  { path: 'Menu', component: MenuComponent }
+  { path: 'Menu', component: MenuComponent },
+  { path: 'Grupos', component: GroupsComponent }
 ];
 
 
--- src/app/configuracion/groups/groups/groups.component.ts
+++ src/app/configuracion/groups/groups/groups.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuEditComponent } from '../../menu/menu-edit/menu-edit.component';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { WebservicesService } from '../../../services/webservices.service';
+import { ModalspinnerComponent } from '../../../shared/modalspinner/modalspinner.component';
 
 @Component({
   selector: 'app-groups',
@@ -6,10 +11,121 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
+  @ViewChild(MenuEditComponent)
+  private editQueryChild: MenuEditComponent;
 
-  constructor() { }
+
+  basicForm;
+
+  title = "Búsqueda";
+
+  filter = "";
+  status = 1;
+  resultLst = [];
+
+  search = true;
+  edit = false;
+  viewFilter = false;
+
+  id = '0';
+  editQuery = 0;
+
+  constructor(private fb: FormBuilder, public dialog: MatDialog, private webservices: WebservicesService) { 
+  this.basicForm = fb.group ({
+    filter: ["",[] ],
+  });
+  }
 
   ngOnInit() {
+  
   }
+
+  /*
+  onChanges(): void {
+  this.basicForm.get('filter').valueChanges.subscribe(val => {
+    this.filter = val;
+  });
+  }
+  */
+
+  doSearch() {
+    this.resultLst = [];
+
+    let dialogRef = this.dialog.open(ModalspinnerComponent,  {
+      width: '250px',
+      disableClose: true,
+      panelClass: 'spinner-dialog'
+      //data: { name: this.name, animal: this.animal }
+    });
+    
+    var model = {
+      filter : this.basicForm.get('filter').value,
+      status : this.status
+    }
+    
+    this.webservices.postMessage("api/Configuration/MenuSearchQuery", model)
+    .then( data => {
+      if (data.error == null) {
+        this.resultLst = data;
+      }
+      
+      dialogRef.close();
+    }).catch( err => {
+      dialogRef.close();
+    });
+  
+  }
+
+  doNuevo() {
+    this.id = '0';
+    this.search = false;
+    this.edit = true;
+  }
+
+
+  onSearch(model) {
+    if (model.typeOperation == 0) {
+      var tmpmodel = {
+        id : model.id,
+        menu : model.menu,
+        status : 1
+      }  
+      this.resultLst.splice(0, 0, tmpmodel);
+    }   
+    else if (model.typeOperation == 1) {
+      for (var x = 0; x < this.resultLst.length; x++) {
+        var tmp = this.resultLst[x] as any;
+        if (tmp.id == model.id) {
+            this.resultLst[x].menu = model.menu;
+        } 
+      }  
+    }
+    this.edit = false;
+    this.search = true;
+  }
+
+  onEditQuery(model) {
+    this.id = model.id;
+    this.editQuery = model.editQuery;
+    this.search = false;
+    this.edit = true;
+  }
+
+  onGoSearch(status) {
+    this.onToggleFiltro();
+    this.status = status;
+    this.doSearch();
+  }
+
+  toggleFiltro() {
+    this.search = false;
+    this.viewFilter = true;
+  }
+
+  onToggleFiltro() {
+    this.search = true;
+    this.viewFilter = false;
+  }
+
 
 }
